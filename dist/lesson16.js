4ee5a1b74175f9a49f6c7fc984c8ade6fdd4b4da
anhtran <anhtqt103@gmail.com>
2020-09-09 10:30:56 +0700

    lesson16-1

--- dist/lesson16.js
+++ dist/lesson16.js
@@ -3,4 +3,13 @@ var colorNames = ['red', 'green', 'blue', 'yellow', 'purple'];
 for (var i_1 in colorNames) {
     console.log(colorNames[i_1]);
 }
+for (var _i = 0, colorNames_1 = colorNames; _i < colorNames_1.length; _i++) {
+    var item = colorNames_1[_i];
+    console.log(item);
+}
+var name2 = "tedu";
+for (var _a = 0, name2_1 = name2; _a < name2_1.length; _a++) {
+    var c = name2_1[_a];
+    console.log(c);
+}
 //# sourceMappingURL=lesson16.js.map
--- lesson16.ts
+++ lesson16.ts
@@ -3,3 +3,12 @@ var colorNames = ['red', 'green', 'blue', 'yellow', 'purple'];
 for (let i in colorNames) {
     console.log(colorNames[i]);
 }
+
+for (let item of colorNames) {
+    console.log(item);
+}
+
+let name2 = "tedu";
+for (let c of name2) {
+    console.log(c);
+}
