0c8aa3822c0b525f9a0f01647d86fc658fa3f7b4
anhtran <anhtqt103@gmail.com>
2020-09-09 10:26:38 +0700

    lesson16

---
+++ dist/lesson16.js
@@ -0,0 +1,6 @@
+"use strict";
+var colorNames = ['red', 'green', 'blue', 'yellow', 'purple'];
+for (var i_1 in colorNames) {
+    console.log(colorNames[i_1]);
+}
+//# sourceMappingURL=lesson16.js.map
---
+++ lesson16.ts
@@ -0,0 +1,5 @@
+var colorNames = ['red', 'green', 'blue', 'yellow', 'purple'];
+
+for (let i in colorNames) {
+    console.log(colorNames[i]);
+}
